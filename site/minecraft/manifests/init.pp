1831779a0c9e1114bcbb84c7edfd9917f3156705
Belén <38337779+rooneyshuman@users.noreply.github.com>
2020-04-23 18:21:14 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
   $install_dir = '/opt/minecraft'
 ){
-  file {$install_dir :
+  file { $install_dir :
     ensure => directory,
   }
   file {"${install_dir}/server.jar":
@@ -19,7 +19,7 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure  => file,
-    content => epp('minecraft/minecraft.service',{
+    content => epp('minecraft/minecraft.service.epp',{
       install_dir => $install_dir,
     })
   }
