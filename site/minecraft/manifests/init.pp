0a09de6b048ce5b54967d31726589abb4bb0f408
Belén <38337779+rooneyshuman@users.noreply.github.com>
2020-04-23 18:29:05 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,27 +5,25 @@ class minecraft (
   file { $install_dir :
     ensure => directory,
   }
-  file {"${install_dir}/server.jar":
+  file { "${install_dir}/server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft'],
   }
-  package {'java':
+  package { 'java':
     ensure  => present,
   }
-  file {"${install_dir}/eula.txt":
+  file { "${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
-    content => epp('minecraft/minecraft.service.epp',{
+    content => epp('minecraft/minecraft.service.epp', {
       install_dir => $install_dir,
-    })
+    }),
   }
-  service {'minecraft':
+  service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
